43b10fbbf76845a84a587fd23898265bcecd3db7
vishalam36 <vishal_vr3@yahoo.in>
2021-03-02 16:00:16 +0530

    Day-04 (U_IgneousTech => FE Web Developer BC => Section-11)

--- 03- FrontEnd-BC_Igneous-Tech (U)/JavaScript/script.js
+++ 03- FrontEnd-BC_Igneous-Tech (U)/JavaScript/script.js
@@ -1,3 +1,21 @@
-<Script>
-    
-</Script>
+// alert("Hello World !")
+
+
+
+var temp = 12;
+
+var myNumber = 50;         //Number
+
+var myString = "Igneous";     //String
+
+var harley = ["street750", "fatboy", "streetbob"];   //Array
+
+var bikeObject = {nameOfBike: "street750", ccOfBike: 750}; //Object
+
+var isActive = true;       //Boolean
+var isComplete = false;    //Boolean
+
+var someString = "";
+
+
+document.getElementById("test").innerHTML = typeof(harley);
